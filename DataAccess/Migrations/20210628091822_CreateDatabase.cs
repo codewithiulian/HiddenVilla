d46f14a8591df8b22554549f666b1bc658e17f9d
Iulian Oana <iulian.andreioana@gmail.com>
2021-06-28 10:21:19 +0100

    56. Create Database

---
+++ DataAccess/Migrations/20210628091822_CreateDatabase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class CreateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // To add this I opened PM console
+            // ran 'add-migration CreateDatabase'
+            // then 'update-database'
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
