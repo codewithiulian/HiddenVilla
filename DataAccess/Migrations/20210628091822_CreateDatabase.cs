e24b1b7ea1907fad8bc7e03864d5e0bd627fbbd5
Iulian Oana <iulianoana@qes-online.com>
2021-06-30 11:52:08 +0100

    72. JS in Blazor

--- DataAccess/Migrations/20210628091822_CreateDatabase.cs
+++ DataAccess/Migrations/20210628091822_CreateDatabase.cs
@@ -9,6 +9,9 @@ namespace DataAccess.Migrations
             // To add this I opened PM console
             // ran 'add-migration CreateDatabase'
             // then 'update-database'
+            // or from PS:
+            // dotnet-ef database update --project .\HiddenVilla_Server\
+            // run the app: dotnet watch run --project .\HiddenVilla_Server\ -debug
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
