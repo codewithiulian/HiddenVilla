7ffa55f48eddc11e459a1a17ca7cbd1fdd68a28d
Iulian Oana <iulian.andreioana@gmail.com>
2021-07-03 14:18:32 +0100

    82. Hotel Room Image Repository Interface.

---
+++ Business/Repository/HotelRoomImageRepository.cs
@@ -0,0 +1,33 @@
+﻿using Business.Repository.IRepository;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Repository
+{
+    public class HotelRoomImageRepository : IHotelRoomImageRepository
+    {
+        public Task<int> CreateHotelRoomImage(HotelRoomImageDTO image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteHotelRoomImageByImageId(int imageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteHotelRoomImageByRoomId(int roomId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<HotelRoomImageDTO>> GetHotelRoomImages(int roomId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
